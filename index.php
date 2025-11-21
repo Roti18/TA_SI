543858874adba4b1813cb823ec8e77ee6f23c8d5
Roti18 <akuronif@gmail.com>
2025-11-22 01:55:46 +0700

    set up tailwind

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require_once 'includes/header.php'; ?>
+
+<h1 class="text-3xl font-bold underline text-center text-red-500">
+  Hello world!
+</h1>
+
+<?php require_once 'includes/footer.php'; ?>
