9f85fe42a1223804d4b068d4ec1fb39944c1f018
Roti18 <akuronif@gmail.com>
2025-11-22 10:51:10 +0700

    add: routing

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+$request = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+$routes = [
+    'home'      => 'home.php',
+    'dashboard' => 'admin.php',
+    'login'     => 'login.php',
+    'profile'   => 'user/profil.php'
+];
+
+if (array_key_exists($request, $routes)) {
+    include $routes[$request];
+} else {
+    http_response_code(404);
+    echo "<h1>Halaman tidak ditemukan! (404)</h1>";
+}
+?>
 <?php require_once 'includes/header.php'; ?>
 
 <h1 class="text-3xl font-bold underline text-center text-red-500">
