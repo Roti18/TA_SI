db04a5bdc66fa9d87d9e602ec281c8db05e8e94e
Roti18 <akuronif@gmail.com>
2025-11-22 10:55:20 +0700

    add: file route

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 $request = isset($_GET['page']) ? $_GET['page'] : 'home';
 
 $routes = [
-    'home'      => 'home.php',
-    'dashboard' => 'admin.php',
-    'login'     => 'login.php',
-    'profile'   => 'user/profil.php'
+    'home'      => 'views/home.php',
+    'dashboard' => 'views/admin.php',
+    'login'     => 'views/login.php',
+    'profile'   => 'views/user/profil.php'
 ];
 
 if (array_key_exists($request, $routes)) {
@@ -15,10 +15,3 @@ if (array_key_exists($request, $routes)) {
     echo "<h1>Halaman tidak ditemukan! (404)</h1>";
 }
 ?>
-<?php require_once 'includes/header.php'; ?>
-
-<h1 class="text-3xl font-bold underline text-center text-red-500">
-  Hello world!
-</h1>
-
-<?php require_once 'includes/footer.php'; ?>
